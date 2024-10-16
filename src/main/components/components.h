b3cf7978422b79e2507207e350fb5387763a1db3
jackychip <jackychipersonal@gmail.com>
2024-10-16 15:32:04 -0400

    Moved components class and added CPP file.

--- src/main/components/components.h
+++ src/main/components/components.h
@@ -22,33 +22,13 @@ public:
     * @param `number_of_pins` number of elements in the modes[] and pin_numbers[] arrays.
     * @param `component_name` name of this component as a string.
     */
-  Component(int pin_numbers[], uint8_t modes[], int number_of_pins, String component_name) 
-  : num_pins(number_of_pins), name(component_name), pin_nums(pin_numbers), mds(modes) {
-
-  };
+  Component(int pin_numbers[], uint8_t modes[], int number_of_pins, String component_name);
 
 private:
-  // TODO: Comment these
   const int num_pins;
   const String name;
   const int *pin_nums;
   const uint8_t *mds;
 };
-
-// TODO: Comment this function
-void Component::setUp() {
-  Serial.print("Initializing ");
-  Serial.print(name);
-  Serial.println("...");
-  Serial.println();
-
-  for (int i = 0; i < num_pins; i++) {
-    Serial.print("Initializing pin ");
-    Serial.print(pin_nums[i]);
-    Serial.println();
-
-    pinMode(pin_nums[i], mds[i]);
-  }
-}
 
 #endif
