ba7272b609ad2ff499e2164fe63585e57764c4cc
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-02-10 10:47:59 -0500

    Changed where python file is

--- src/python/ir-emulator.py
+++ src/python/ir-emulator.py
@@ -7,16 +7,17 @@ WIDTH, HEIGHT = 600, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-center = (300, 300)  
-radius = 100         
-num_circles = 16    
+center = (300, 300)
+radius = 100
+num_circles = 16
 circle_radius = 10
 
-brightness_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4] #test values
+brightness_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8,
+                     0.9, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4]  # test values
 
 running = True
 while running:
-    screen.fill("white") 
+    screen.fill("white")
     pygame.draw.circle(screen, "black", (300, 300), 100)
     for i in range(num_circles):
         angle = (i / num_circles) * 2 * math.pi  # Calculate angle
@@ -29,12 +30,12 @@ while running:
 
         pygame.draw.circle(screen, color, (x, y), circle_radius)
 
-    pygame.display.flip() 
+    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    clock.tick(60) 
+    clock.tick(60)
 
 pygame.quit()
