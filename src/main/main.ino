ebbab598985b000d94d3458168e0d8bac8c2ec6c
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-04-02 19:56:53 -0400

    Update main.ino

--- src/main/main.ino
+++ src/main/main.ino
@@ -15,6 +15,30 @@ float angularDifference(float a, float b) {
   return (diff > 180) ? 360 - diff : diff;
 }
 
+void attack_w_color_sensor() {
+  int speed = 185;
+  if (m.is_on_border()) {
+    shouldStop = true;
+  } 
+
+  if (!shouldStop) {
+    m.move(ballAngle, speed);
+  } else {
+    float stopped_robot_ball_angle = ir.getBallAngle();
+    float curr_ball_angle = stopped_robot_ball_angle;
+    
+    while (angularDifference(stopped_robot_ball_angle, curr_ball_angle) < 20) {
+      curr_ball_angle = ir.getBallAngle();
+      m.brake();
+      delay(50);
+    }
+    
+    shouldStop = false;
+    m.move(ballAngle, speed);
+    delay(300);
+  }
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -23,7 +47,7 @@ void setup() {
 }
 
 void loop() {
-  int speed = 210;
+  int speed = 185;
   float ballAngle = ir.getBallAngle();
   // Serial.print("ballAngle: ");
   // Serial.println(ballAngle > 180 ? ballAngle - 360 : ballAngle);
@@ -49,12 +73,15 @@ void loop() {
   //   delay(300);
   // }
 
-  // m.move(0, speed);
+  m.move(0, speed);
 
   // m.debug_sees_border();
   
   // m.debug();
   delay(50);
 }
+
+
+
 
 
